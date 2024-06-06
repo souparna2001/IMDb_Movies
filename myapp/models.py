f83b0f32a4d7136fc43d691de24a450313a4e6b4
souparna <souparna7921@gmail.com>
2024-06-06 21:37:09 +0530

    message

---
+++ myapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    name=models.CharField(max_length=200,unique=True)
+    language=models.CharField(max_length=200)
+    runtime=models.PositiveIntegerField()
+    genre=models.CharField(max_length=200)
+    director=models.CharField(max_length=200)
+    year=models.PositiveIntegerField()
+    actors=models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
